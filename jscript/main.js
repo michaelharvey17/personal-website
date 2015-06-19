146676abde05fc1c9fbaaf5dd5626cc3a40b7bd0
Michael Harvey <michaelharvey17@gmail.com>
2015-06-19 17:28:10 -0400

    Added some hover effects altered colors slightly and made links on contact page work.

---
+++ jscript/main.js
@@ -0,0 +1,11 @@
+onload=function(){
+}
+function scriptchange() {
+	document.getElementById("script").data ="media/Symptoms_of_Insanity_Fight_Scene.pdf"; 
+	document.getElementById("script-title").innerHTML="Symptoms of Insanity (Action)";
+}
+function scriptchange2() {
+	document.getElementById("script").data ="media/Cold_Turkey_Part1.pdf"; 
+	document.getElementById("script-title").innerHTML="Cold Turkey Part1 (Comedy)";
+}
+
